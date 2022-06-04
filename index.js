abcd090b037c925d78115c0e4221ae3025b29594
kelsonbatista <kelsonbatista@gmail.com>
2022-06-03 22:01:10 -0300

    refactor(redeploy): simple commit just to redeploy

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
-
+ 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
